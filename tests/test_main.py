e97567b4eea324f465d82e4b274bb765047cbe58
Henrik Holm <ukrutt@gmail.com>
2020-01-07 16:35:23 -0500

    Oh noes, my test fails because of floating point arithmetic

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from pytest_mix_dict import main
 
 def test_add_two():
     """Sample test routine."""
-    arg_x = 2
-    arg_y = 3
+    arg_x = 0.2
+    arg_y = 0.1
     ans_z = main.add_two(arg_x, arg_y)
-    assert ans_z == 5
+    assert ans_z == 0.3
