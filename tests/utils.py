6a505bff578af83f4415d73f01b2b4a219f3dfa3
Henrik Holm <ukrutt@gmail.com>
2020-01-08 10:02:45 -0500

    Cleanup and comments

--- tests/utils.py
+++ tests/utils.py
@@ -7,19 +7,20 @@ from _pytest.python_api import ApproxMapping
 # from _pytest.python_api import approx as _pytest_approx
 
 
-class ApproxMappingFlex(  # pylint: disable=too-few-public-methods
-    ApproxMapping
-):
+class ApproxMappingFlex(ApproxMapping):
     """More flexible ApproxMapping.
 
     This one will accept dicts with non-numeric values.
     """
+
+    # pylint: disable=too-few-public-methods
     def _check_type(self):
-        __tracebackhide__ = True
-        for key, value in self.expected.items():
+        # TODO: check if this next variable is in fact necessary.
+        __tracebackhide__ = True  # pylint: disable=unused-variable
+        for _key, value in self.expected.items():
             if isinstance(value, type(self.expected)):
-                msg = "pytest.approx() does not support nested dictionaries: key={!r} value={!r}\n  full mapping={}"
-                raise TypeError(msg.format(key, value, pprint.pformat(self.expected)))
+                # Defer this error check to the original ApproxMapping
+                super()._check_type()
 
 
 def dict_approx(expected, rel=None, absolute=None, nan_ok=False):
