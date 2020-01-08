ea17977e75e6b0b4065bf017e1b300f103399c95
Henrik Holm <ukrutt@gmail.com>
2020-01-08 09:48:44 -0500

    Class stub

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,25 @@
 """Test utilities."""
 
-from _pytest.python_api import approx as _pytest_approx
+from collections.abc import Mapping
 
-dict_approx = _pytest_approx  # pylint: disable=invalid-name
+from _pytest.python_api import ApproxMapping
+
+# from _pytest.python_api import approx as _pytest_approx
+
+
+class ApproxMappingFlex(  # pylint: disable=too-few-public-methods
+    ApproxMapping
+):
+    """More flexible ApproxMapping.
+
+    This one will accept dicts with non-numeric values.
+    """
+
+
+def dict_approx(expected, rel=None, absolute=None, nan_ok=False):
+    """Approximate for dicts."""
+    if not isinstance(expected, Mapping):
+        raise TypeError(
+            "Can only use this method for a dict or another mapping"
+        )
+    return ApproxMappingFlex(expected, rel, absolute, nan_ok)
